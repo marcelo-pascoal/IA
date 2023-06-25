a2a6c6fd3acacdbe0bd8cdb4af77e90a67da30d6
marcelo-pascoal <132770891+marcelo-pascoal@users.noreply.github.com>
2023-06-25 16:45:58 +0100

    Revert "Revert "Recombinações""
    This reverts commit 2fad075ff40ad45ffcafd4c9edd08dc6a0f7a4d2.

--- ga/genetic_operators/recombination_pmx.py
+++ ga/genetic_operators/recombination_pmx.py
@@ -3,7 +3,7 @@ from ga.individual import Individual
 from ga.genetic_algorithm import GeneticAlgorithm
 
 
-class Recombination2(Recombination):
+class RecombinationPMX(Recombination):
 
     def __init__(self, probability: float):
         super().__init__(probability)
@@ -21,21 +21,15 @@ class Recombination2(Recombination):
             mapping1[ind1.genome[i]] = ind2.genome[i]
             mapping2[ind2.genome[i]] = ind1.genome[i]
 
-        # Versao genoma dominante: quando deteta colisoes preenche com o genoma do ind com melhor fitness
+        # Versao Original: quando deteta colisoes preenche por ordem sequencial o genoma nao presente do ind 'principal'
         child1 = [-1] * len(ind1.genome)
         for i in range(cut1, cut2 + 1):
             child1[i] = ind1.genome[i]
         for i in range(len(ind1.genome)):
             if child1[i] == -1:
                 gene = ind2.genome[i]
-                if gene in mapping1:
-                    if ind1.fitness < ind2.fitness:
-                        gene = ind1.genome[i]
-                    else:
-                        for index, gene1 in enumerate(ind1.genome[cut1:], start=cut1):
-                            if gene1 == gene:
-                                break
-                        child1[index] = ind2.genome[index]
+                while gene in mapping1:
+                    gene = mapping1[gene]
                 child1[i] = gene
 
         child2 = [-1] * len(ind1.genome)
@@ -44,14 +38,8 @@ class Recombination2(Recombination):
         for i in range(len(ind1.genome)):
             if child2[i] == -1:
                 gene = ind1.genome[i]
-                if gene in mapping2:
-                    if ind2.fitness < ind1.fitness:
-                        gene = ind2.genome[i]
-                    else:
-                        for index, gene2 in enumerate(ind2.genome[cut1:], start=cut1):
-                            if gene2 == gene:
-                                break
-                        child2[index] = ind1.genome[index]
+                while gene in mapping2:
+                    gene = mapping2[gene]
                 child2[i] = gene
 
         ind1.genome = child2
